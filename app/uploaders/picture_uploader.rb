f3e9ec24930e2edeca9f0a72e9070d0e30fcb68b
bennycazares <benny.cazares@gmail.com>
2016-08-09 14:17:43 -0500

    none

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -11,8 +11,6 @@ class PictureUploader < CarrierWave::Uploader::Base
    storage :file
   end
 
-  storage :file
-  # storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
