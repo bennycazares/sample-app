e84cf844f6204cab3435b20c3e7358d8b4c5c932
bennycazares <benny.cazares@gmail.com>
2016-08-09 14:30:08 -0500

    none

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,12 +1,9 @@
-if Rails.env.production?
   CarrierWave.configure do |config|
     config.fog_credentials = {
       # Configuration for Amazon S3
       :provider              => 'AWS',
-      :aws_access_key_id     => ENV['AKIAIGOSBKLK26A5YMTQ'],
-      :aws_secret_access_key => ENV['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']
-      :region                => ENV['us-east-1']
+      :aws_access_key_id     => 'AKIAIGOSBKLK26A5YMTQ',
+      :aws_secret_access_key => 'AD6Yr6LP0Vji28fGU8SVc/oYm8UwJ+ioaVPnPaFp'
     }
-    config.fog_directory     =  ENV['sample-app32']
+    config.fog_directory     =  'sample-app32'
   end
-end
